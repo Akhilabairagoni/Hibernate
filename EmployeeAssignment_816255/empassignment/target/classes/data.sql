03a2e25111692c8e099a3c9110b6889f57bee1d1
Akhilabairagoni <100116182+Akhilabairagoni@users.noreply.github.com>
2022-03-04 14:39:08 +0530

    Add files via upload

---
+++ EmployeeAssignment_816255/empassignment/target/classes/data.sql
@@ -0,0 +1,30 @@
+insert into employee(id, name, last_name) 
+values(1000,'soumya','gajula');
+insert into employee (id, name, last_name) 
+values(1001,'vaishnavi' ,'thatla' );
+insert into employee(id, name, last_name ) 
+values(1002,'priya', 'shetty');
+insert into employee (id, name, last_name) 
+values(1003,'shravya' ,'kisari' );
+insert into employee (id, name, last_name) 
+values(1004,'akanksha' ,'sirigiri' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
